acc02c7235b8736014bf9b69f954191a7eb4eadc
Joseph Brennan <jbrenna9@nd.edu>
2019-07-09 16:34:30 -0400

    testing

--- spinup.R
+++ spinup.R
@@ -14,11 +14,13 @@ for(i in 1:100){
   temps_target$index = dates[[1]][[4*i]]
   d = rbind(d, temps_target)
 }
+##Varying With Depth? - less deep less spinup required, on the other hand deeper temps are less volatile
+#2 depth first ~73 predictions above 18, 8 depth first ~24 predictions above 18, 16 depth only first 7 above 10
 
-
+f = NULL
 for(i in 1:100){     ## breakdown at 4/28 not sure why -> very close to first temp obs 4/30
 
-  glm_nml <- set_nml(glm_nml, arg_list = list('start' = dates[[1]][[75 + 4*i]],'stop' =  '1980-04-27 00:00:00'))
+  glm_nml <- set_nml(glm_nml, arg_list = list('start' = dates[[1]][[75 + 4*i]],'stop' =  '1980-04-30 00:00:00'))
   write_nml(glm_nml, file = 'sim_raw/glm2.nml')
   sim_out = run_simulation(config_path = 'glm_nml',
                            orig_meteo_file = 'mendota_driver_data.csv',
@@ -26,8 +28,8 @@ for(i in 1:100){     ## breakdown at 4/28 not sure why -> very close to first te
                            meteo_dir = '1_data/in')
 
   temps <- get_temp(file.path('sim_raw', 'output.nc'), reference = 'surface', z_out = c(2,8,16))
-  temps_target <- temps[which(temps$DateTime == '1980-04-27'), ]
+  temps_target <- temps[which(temps$DateTime == '1980-04-30'), ] #breakdown here
   temps_target$index = dates[[1]][[75 + 4*i]]
-  d = rbind(d, temps_target)
+  f = rbind(f, temps_target)
 }
 
